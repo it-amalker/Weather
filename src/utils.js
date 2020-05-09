cf933ba393e95fcc83e84500b37f0869636a1f8a
amalker <it.amalker@yandex.ru>
2020-05-09 18:12:00 +0300

    add debounce

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+const debounce = () => {
+  let setTimeoutId;
+
+  return (f, delay) => {
+    if (setTimeoutId) {
+      clearTimeout(setTimeoutId);
+    }
+    setTimeoutId = setTimeout(() => {
+      f();
+      setTimeoutId = null;
+    }, delay);
+  };
+};
+
+export default debounce;
