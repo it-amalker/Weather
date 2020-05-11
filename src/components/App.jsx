9ee07f560818a2db9f1f476cf3fcf8377a94dbd1
amalker <it.amalker@yandex.ru>
2020-05-11 17:55:19 +0300

    add weather state, change html structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,23 @@
 // @ts-check
 
-import React from 'react';
-import CitiesInput from './CitiesInput';
+import React, { useState } from 'react';
+import CitiesSearch from './CitiesSearch';
+import WeatherInfo from './WeatherInfo';
 
-const App = () => (
-  <div>
-    <CitiesInput />
-  </div>
-);
+const App = () => {
+  const [weather, setWeather] = useState(null);
+  console.log(weather);
+
+  return (
+    <>
+      <div className="search-side">
+        <CitiesSearch weather={weather} setWeather={setWeather} />
+      </div>
+      <div className="result-side">
+        <WeatherInfo weather={weather} />
+      </div>
+    </>
+  );
+};
 
 export default App;
