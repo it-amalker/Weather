cda1b9531dee23fc15b8c6648e89bad36f0ce967
amalker <it.amalker@yandex.ru>
2020-05-11 17:56:36 +0300

    add options to check weather by coordinates

--- src/routesAPI.js
+++ src/routesAPI.js
@@ -2,18 +2,19 @@
 
 const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
-const getCitiesAPI = (city, limit = 7, format = 'json', language = 'en-US') => {
+const getCities = (city, limit = 7, format = 'json', language = 'en-US') => {
   const host = 'https://nominatim.openstreetmap.org/';
   const params = `city=${city}&format=${format}&limit=${limit}&accept-language=${language}`;
 
   return [host, params].join('?');
 };
 
-const getWeatherAPI = (city, units = 'metric') => {
+const getWeather = ({ city, coordinates: { lat, lon }, units = 'metric' }) => {
+  const query = city ? `q=${city}` : `lat=${lat}&lon=${lon}`;
   const host = 'https://api.openweathermap.org/data/2.5/weather';
-  const params = `q=${city}&units=${units}&appid=${WEATHER_API_KEY}`;
+  const params = `${query}&units=${units}&appid=${WEATHER_API_KEY}`;
 
   return [host, params].join('?');
 };
 
-export { getCitiesAPI, getWeatherAPI };
+export { getCities, getWeather };
