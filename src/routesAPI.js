fbcec1b10a4481aebc967e810fab431af4804f7d
amalker <it.amalker@yandex.ru>
2020-05-12 18:13:25 +0300

    small refactor

--- src/routesAPI.js
+++ src/routesAPI.js
@@ -2,17 +2,17 @@
 
 const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
-const getCities = (city, limit = 7, format = 'json', language = 'en-US') => {
+const getCities = (city, limit = 7, language = 'en') => {
   const host = 'https://nominatim.openstreetmap.org/';
-  const params = `city=${city}&format=${format}&limit=${limit}&accept-language=${language}`;
+  const params = `city=${city}&format=json&limit=${limit}&accept-language=${language}`;
 
   return [host, params].join('?');
 };
 
-const getWeather = ({ city, coordinates: { lat, lon }, units = 'metric' }) => {
-  const query = city ? `q=${city}` : `lat=${lat}&lon=${lon}`;
+const getWeather = ({ city, coordinates }) => {
+  const query = city ? `q=${city}` : `lat=${coordinates.lat}&lon=${coordinates.lon}`;
   const host = 'https://api.openweathermap.org/data/2.5/weather';
-  const params = `${query}&units=${units}&appid=${WEATHER_API_KEY}`;
+  const params = `${query}&units=metric&appid=${WEATHER_API_KEY}`;
 
   return [host, params].join('?');
 };
