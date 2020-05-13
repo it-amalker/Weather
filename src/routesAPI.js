9ab3001c89acbb6cbec94cd5d24a32bd88dd88e2
amalker <it.amalker@yandex.ru>
2020-05-13 18:00:47 +0300

    small refactor

--- src/routesAPI.js
+++ src/routesAPI.js
@@ -2,19 +2,22 @@
 
 const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
+const openStreetApiPath = 'https://nominatim.openstreetmap.org/';
+const weatherApiPath = 'https://api.openweathermap.org/data/2.5/weather';
+
 const getCities = (city, limit = 7, language = 'en') => {
-  const host = 'https://nominatim.openstreetmap.org/';
   const params = `city=${city}&format=json&limit=${limit}&accept-language=${language}`;
-
-  return [host, params].join('?');
+  return [openStreetApiPath, params].join('?');
 };
 
-const getWeather = ({ city, coordinates }) => {
-  const query = city ? `q=${city}` : `lat=${coordinates.lat}&lon=${coordinates.lon}`;
-  const host = 'https://api.openweathermap.org/data/2.5/weather';
-  const params = `${query}&units=metric&appid=${WEATHER_API_KEY}`;
-
-  return [host, params].join('?');
+const getWeatherByName = (city) => {
+  const params = `q=${city}&units=metric&appid=${WEATHER_API_KEY}`;
+  return [weatherApiPath, params].join('?');
 };
 
-export { getCities, getWeather };
+const getWeatherByCoordinates = ({ lat, lon }) => {
+  const params = `lat=${lat}&lon=${lon}&units=metric&appid=${WEATHER_API_KEY}`;
+  return [weatherApiPath, params].join('?');
+};
+
+export { getCities, getWeatherByName, getWeatherByCoordinates };
