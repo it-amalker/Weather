809134cf0bc173a36bf9a3e95a4c5df73baf48a0
amalker <it.amalker@yandex.ru>
2020-05-15 18:51:02 +0300

    add types

--- src/routesAPI.ts
+++ src/routesAPI.ts
@@ -3,17 +3,22 @@ const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const openStreetApiPath = 'https://nominatim.openstreetmap.org/';
 const weatherApiPath = 'https://api.openweathermap.org/data/2.5/weather';
 
-const getCities = (city, limit = 7, language = 'en') => {
+const getCities = (city: string, limit: number | string = 7, language: string = 'en'): string => {
   const params = `city=${city}&format=json&limit=${limit}&accept-language=${language}`;
   return [openStreetApiPath, params].join('?');
 };
 
-const getWeatherByName = (city) => {
+const getWeatherByName = (city: string): string => {
   const params = `q=${city}&units=metric&appid=${WEATHER_API_KEY}`;
   return [weatherApiPath, params].join('?');
 };
 
-const getWeatherByCoordinates = ({ lat, lon }) => {
+interface Coordinates {
+  lat: string;
+  lon: string;
+}
+
+const getWeatherByCoordinates = ({ lat, lon }: Coordinates) => {
   const params = `lat=${lat}&lon=${lon}&units=metric&appid=${WEATHER_API_KEY}`;
   return [weatherApiPath, params].join('?');
 };
