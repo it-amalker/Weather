961902e4629d2ba97f41f6c9d3c8d491394b68e2
amalker <it.amalker@yandex.ru>
2020-05-17 21:20:11 +0300

    fix config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,30 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-
-  plugins: [
-    "@typescript-eslint",
-    "react",
-    "import",
-    "jsx-a11y",
-  ],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 
   extends: [
-    "airbnb-typescript",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/react",
-    "prettier/@typescript-eslint",
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
 
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   env: {
     node: true,
     browser: true,
-    rules: {
-      "no-console": "off",
-      "react/prop-types": "off",
-    },
-  }
-}
+  },
+  rules: {
+    'no-console': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+};
