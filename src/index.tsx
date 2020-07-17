4205b265b47b00d99619ee9549350d0415a4529d
amalker <it.amalker@yandex.ru>
2020-07-17 19:11:43 +0300

    add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import GlobalStyle from './components/global/globalStyles';
 
 import './styles/style.css';
 import 'normalize.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root'),
+);
