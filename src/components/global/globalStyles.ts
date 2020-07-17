48594c9abc130731ce760b91ab186eae01d7303a
amalker <it.amalker@yandex.ru>
2020-07-17 19:14:38 +0300

    add global styles and font imports

---
+++ src/components/global/globalStyles.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+
+import kaushanScriptWoff2 from '../../assets/fonts/kaushan-script-regular.woff2';
+import kaushanScriptWoff from '../../assets/fonts/kaushan-script-regular.woff';
+import openSansWoff2 from '../../assets/fonts/open-sans-regular.woff2';
+import openSansWoff from '../../assets/fonts/open-sans-regular.woff';
+import openSansBoldWoff2 from '../../assets/fonts/open-sans-600.woff2';
+import openSansBoldWoff from '../../assets/fonts/open-sans-600.woff';
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Kaushan Script';
+    src: url(${kaushanScriptWoff2}) format('woff2'),
+      url(${kaushanScriptWoff}) format('woff');
+    font-weight: 400;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'Open Sans';
+    src: url(${openSansWoff2}) format('woff2'),
+      url(${openSansWoff}) format('woff');
+    font-weight: 400;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'Open Sans';
+    src: url(${openSansBoldWoff2}) format('woff2'),
+      url(${openSansBoldWoff}) format('woff');
+    font-weight: 600;
+    font-style: bold;
+    font-display: swap;
+  }
+
+  body {
+    box-sizing: border-box;
+  
+    font-family: 'Open Sans', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
