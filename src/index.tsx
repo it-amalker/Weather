b59ec178f74b646b50d6d8267e3c035a25ad689f
amalker <it.amalker@yandex.ru>
2020-07-18 15:06:22 +0300

    delete styles css

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 import GlobalStyle from './components/global/globalStyles';
 
-import './styles/style.css';
-
 ReactDOM.render(
   <>
     <App />
