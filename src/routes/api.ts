9859c70d49c28bde4191f324cd1c82246ec55545
amalker <it.amalker@yandex.ru>
2020-07-18 13:09:27 +0300

    decrease cities limit to 5

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,7 +7,7 @@ const weatherApiPath = 'https://api.openweathermap.org/data/2.5/weather';
 
 const getCities = (
   city: string,
-  limit: number | string = 7,
+  limit: number | string = 5,
   language = 'en',
 ): string => {
   const params = `city=${city}&format=json&limit=${limit}&accept-language=${language}`;
