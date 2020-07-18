9486531eb4f6c384fa1178b407905a567d5ba2d7
amalker <it.amalker@yandex.ru>
2020-07-18 15:00:51 +0300

    change div to section

--- src/components/layout/searchSide/SearchSide.styles.ts
+++ src/components/layout/searchSide/SearchSide.styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const SearchContainer = styled.div`
+const SearchContainer = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
