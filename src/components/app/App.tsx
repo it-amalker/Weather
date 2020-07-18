7b0f14c2630ef2dfe1795934359587065060d048
amalker <it.amalker@yandex.ru>
2020-07-18 13:12:24 +0300

    relocate App component

---
+++ src/components/app/App.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+// Components
+import Search from '../search';
+import WeatherInfo from '../WeatherInfo';
+import Layout from '../layout';
+import SearchSide from '../layout/searchSide';
+import ResultSide from '../layout/resultSide';
+// Types
+import * as WeatherTypes from '../../types/weather';
+
+const App: React.FC = () => {
+  const [weather, setWeather] = useState<WeatherTypes.Description>(null);
+  const [currentCity, setCurrentCity] = useState<string>('');
+
+  return (
+    <Layout>
+      <SearchSide>
+        <Search
+          currentCity={currentCity}
+          setCurrentCity={setCurrentCity}
+          setWeather={setWeather}
+        />
+      </SearchSide>
+      <ResultSide>
+        <WeatherInfo weather={weather} currentCity={currentCity} />
+      </ResultSide>
+    </Layout>
+  );
+};
+
+export default App;
