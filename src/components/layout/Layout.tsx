b3504cc2eb917a0c3c03c922f1b178ed5ff33f3f
amalker <it.amalker@yandex.ru>
2020-07-19 15:15:03 +0300

    add noResponsive component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 
 import { Main, Title } from './Layout.styles';
+import NoResponsive from './noResponsive';
 
 type LayoutProps = {
   children?: ReactNode;
@@ -10,6 +11,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => (
   <Main>
     <Title>Weather App</Title>
     {children}
+    <NoResponsive />
   </Main>
 );
 
