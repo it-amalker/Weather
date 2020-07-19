3761ee93d0c2dc34f76c2ade1ac43408a723afbc
amalker <it.amalker@yandex.ru>
2020-07-19 15:18:59 +0300

    add media queries properties

--- src/components/layout/resultSide/ResultSide.styles.ts
+++ src/components/layout/resultSide/ResultSide.styles.ts
@@ -1,13 +1,17 @@
 import styled from 'styled-components';
 
 const ResultContainer = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 50%;
-  height: 100vh;
+  display: none;
 
-  background-color: #fffafa;
+  @media ${({ theme }) => theme.mediaQueries.desktop} {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    height: 100vh;
+
+    background-color: ${({ theme }) => theme.colors.secondary};
+  }
 `;
 
 export default ResultContainer;
--- src/components/layout/searchSide/SearchSide.styles.ts
+++ src/components/layout/searchSide/SearchSide.styles.ts
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
 
 const SearchContainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 50%;
-  height: 100vh;
+  display: none;
 
-  background-color: #000;
-  box-shadow: 0.2rem 0.2rem 1rem rgba(0, 0, 0, 0.3);
+  @media ${({ theme }) => theme.mediaQueries.desktop} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+    height: 100vh;
 
-  z-index: 1;
+    background-color: ${({ theme }) => theme.colors.main};
+    box-shadow: 0.2rem 0.2rem 1rem rgba(0, 0, 0, 0.3);
+
+    z-index: 1;
+  }
 `;
 
 export default SearchContainer;
